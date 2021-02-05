54c46273d86ed79d0797c7192707207e4c951c7e
Ayo Badejoh <ayobadejoh@gmail.com>
2021-02-05 13:10:21 -0500

    Store result

--- OnesAndZeroes_frontend/src/components/Quiz.js
+++ OnesAndZeroes_frontend/src/components/Quiz.js
@@ -6,35 +6,34 @@ import { PushAnswer } from '../hooks/setResult';
 
 /** redux store import */
 import { useSelector, useDispatch } from 'react-redux'
+import { Navigate } from 'react-router-dom'
 
 export default function Quiz() {
 
     const [check, setChecked] = useState(undefined)
 
-    const state = useSelector(state => state);
+    const result = useSelector(state => state.result.result);
     const { queue, trace } = useSelector(state => state.questions);
     const dispatch = useDispatch()
 
-    useEffect(() => {
-        console.log(state)
-    })
-
     /** next button event handler */
     function onNext(){
-        console.log('On next click')
-
         if(trace < queue.length){
             /** increase the trace value by one using MoveNextAction */
             dispatch(MoveNextQuestion());
 
-            dispatch(PushAnswer(check))
+            /** insert a new result in the array.  */
+            if(result.length <= trace){
+                dispatch(PushAnswer(check))
+            }
         }
-        
+     
+        /** reset the value of the checked variable */
+        setChecked(undefined)
     }
 
     /** Prev button event handler */
     function onPrev(){
-        console.log('On onPrev click')
         if(trace > 0){
             /** decrease the trace value by one using MovePrevQuestion */
             dispatch(MovePrevQuestion());
@@ -46,6 +45,11 @@ export default function Quiz() {
         setChecked(check)
     }
 
+    /** finished exam after the last question */
+    if(result.length && result.length >= queue.length){
+        return <Navigate to={'/result'} replace={true}></Navigate>
+    }
+
   return (
     <div className='container'>
         <h1 className='title text-light'>Quiz Application</h1>
@@ -54,7 +58,7 @@ export default function Quiz() {
         <Questions onChecked={onChecked} />
 
         <div className='grid'>
-            <button className='btn prev' onClick={onPrev}>Prev</button>
+            { trace > 0 ? <button className='btn prev' onClick={onPrev}>Prev</button> : <div></div>}
             <button className='btn next' onClick={onNext}>Next</button>
         </div>
     </div>
